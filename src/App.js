86e2154cca37be1a49b7912dbdc9dbe81b7324da
keebon <keebon@.github.com>
2019-05-14 19:34:55 +0800

    css grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './components/SideBar';
+import Header from './components/templates/Header';
+import Footer from './components/templates/Footer';
+import Content from './components/Body';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <SideBar />
+      <Content />
+      <Footer />
     </div>
   );
 }
