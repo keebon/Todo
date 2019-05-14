539152644cdffe08d3139a8892f9355ffc6251a0
keebon <keebon@.github.com>
2019-05-14 22:02:07 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
 import SideBar from './components/SideBar';
-import Header from './components/templates/Header';
-import Footer from './components/templates/Footer';
 import Content from './components/Body';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <SideBar />
       <Content />
-      <Footer />
     </div>
   );
 }
